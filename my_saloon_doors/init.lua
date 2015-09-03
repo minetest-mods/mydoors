8cefecc4675e6e4e8fcbf79211b1e140ee7cac19
DonBatman <serfdon@gmail.com>
2015-09-03 13:12:57 -0700

    Added saloon doors.

---
+++ my_saloon_doors/init.lua
@@ -0,0 +1,6 @@
+
+dofile(minetest.get_modpath("my_saloon_doors").."/saloon1.lua")
+--dofile(minetest.get_modpath("my_saloon_doors").."/saloon2.lua")
+--dofile(minetest.get_modpath("my_saloon_doors").."/saloon3.lua")
+--dofile(minetest.get_modpath("my_saloon_doors").."/saloon4.lua")
+--dofile(minetest.get_modpath("my_saloon_doors").."/saloon5.lua")
